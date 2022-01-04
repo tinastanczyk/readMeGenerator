9405bdca2e882ae399787f655f394b944081cd38
Tina Stanczyk <cstanczyk123@gmail.com>
2022-01-04 14:49:38 -0500

    Created starter files

---
+++ index.js
@@ -0,0 +1,23 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = ['What is the project title?', 
+'What was your motivation?', 
+'Why did you build this project?', 
+'What problem does it solve?', 
+'What did you learn?', 
+'What are the steps required to install your project?', 
+'Do you want to add a screenshot?', 
+'Who are the collaborators, what third-party assets, or tutorials did you use?', 
+'Choose a license: ', 
+'Does your project have any Badges?', 
+'List your projects features: ', 'How can the user contribute?', 'What tests have you done?'];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
